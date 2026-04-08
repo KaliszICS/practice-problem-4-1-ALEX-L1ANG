4eced8c0586a40ef34ce9640c0d53d11aeee1121
ALEX-L1ANG <343293684@tdsb.ca>
2026-04-08 15:01:42 +0000

    update1

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,27 +1,36 @@
+/*
+File Name: Lesson 4.1 - While Loops
+Author: Alex Liang
+Date Created: April 8, 2026
+Date Last Modified: April 8, 2026
+*/
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 
 	}
 
-	public static void q1() {
-		//Write question 1 code here
-	}
+		public static String seriesInclusive(int num){
+			int i = 1;
+			String series = "";
+			while (i <= num){
+				series = series + i + " ";
+				i++;
+			}
+			series = series.trim();
+			return series;
+		}
 
-	public static void q2() {
-		//Write question 2 code here
-	}
-
-	public static void q3() {
-		//Write question 3 code here
-	}
-
-	public static void q4() {
-		//Write question 4 code here
-	}
-
-	public static void q5() {
-		//Write question 5 code here
-	}
+		public static String seriesExclusive(int num){
+			int i = 0;
+			String series = "";
+			while (i < num){
+				series = series + i + " ";
+				i++;
+			}
+			series = series.trim();
+			return series;
+		}
 
 }
